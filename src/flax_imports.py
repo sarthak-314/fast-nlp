f8c0d10470d58eda30a34e6898c9d9defb1435da
EpicBoiCarti <sarthak.bhatt314@gmail.com>
2022-07-07 15:44:40 +0530

    lazy

--- src/flax_imports.py
+++ src/flax_imports.py
@@ -1,5 +1,6 @@
 from flax.serialization import from_bytes, to_bytes
 from flax import jax_utils, struct, traverse_util
+from flax.jax_utils import replicate, unreplicate
 from flax.training.common_utils import shard
 from flax.training import train_state
 import flax.linen as nn
